e0ee2879c86d1db877f0ad312659e45868db6961
AbelardoReyes <abelardoreyes256@gmail.com>
2024-12-06 16:47:30 -0600

    fix: :bug: Error de vista ListGroupsPage

---
+++ Taskly App/Views/ListGroups/ListGroupsPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace Taskly_App.Views.ListGroups
+{
+    public partial class ListGroupsPage : ContentPage // Nombre de la clase
+    {
+        public ListGroupsPage() // Constructor coincide con la clase
+        {
+            InitializeComponent();
+        }
+    }
+}
